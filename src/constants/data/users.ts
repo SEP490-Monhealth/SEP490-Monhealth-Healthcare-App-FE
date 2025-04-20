6a10a67079d62f9f9806998d750591c2fa2696b4
Van Huu Toan <vhtoan27@gmail.com>
2025-04-21 01:35:59 +0700

    feat: deploy website on netlify

--- src/constants/data/users.ts
+++ src/constants/data/users.ts
@@ -1,132 +1,132 @@
-import { UserType } from "@/schemas/userSchema"
+// import { UserType } from "@/schemas/userSchema"
 
-export const sampleUserData: UserType[] = [
-  {
-    userId: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+// export const sampleUserData: UserType[] = [
+//   {
+//     userId: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
 
-    fullName: "asd",
-    email: "asd@gmail.com",
-    phoneNumber: "0123456789",
-    avatarUrl: null,
+//     fullName: "asd",
+//     email: "asd@gmail.com",
+//     phoneNumber: "0123456789",
+//     avatarUrl: null,
 
-    role: "User",
-    status: true,
+//     role: "User",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "9d7e87a9-b070-4607-a0b0-2d2322aece9b",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "9d7e87a9-b070-4607-a0b0-2d2322aece9b",
 
-    fullName: "Van Huu Toan",
-    email: "toanvan@gmail.com",
-    phoneNumber: "0792766979",
-    avatarUrl: null,
+//     fullName: "Van Huu Toan",
+//     email: "toanvan@gmail.com",
+//     phoneNumber: "0792766979",
+//     avatarUrl: null,
 
-    role: "User",
-    status: true,
+//     role: "User",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "277ea066-d041-40ff-9dae-6271dbd6fd87",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "277ea066-d041-40ff-9dae-6271dbd6fd87",
 
-    fullName: "Nguyen Quoc Dai",
-    email: "quocdai@gmail.com",
-    phoneNumber: "0932748924",
-    avatarUrl: null,
+//     fullName: "Nguyen Quoc Dai",
+//     email: "quocdai@gmail.com",
+//     phoneNumber: "0932748924",
+//     avatarUrl: null,
 
-    role: "Consultant",
-    status: true,
+//     role: "Consultant",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "3b1a8845-765f-4d91-984a-4e8a9d7d376e",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "3b1a8845-765f-4d91-984a-4e8a9d7d376e",
 
-    fullName: "Phan Van Khai",
-    email: "khaitoi@gmail.com",
-    phoneNumber: "0963122758",
-    avatarUrl: null,
+//     fullName: "Phan Van Khai",
+//     email: "khaitoi@gmail.com",
+//     phoneNumber: "0963122758",
+//     avatarUrl: null,
 
-    role: "User",
-    status: true,
+//     role: "User",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "1246b8e5-af73-4aa3-bdef-b8815e21a78b",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "1246b8e5-af73-4aa3-bdef-b8815e21a78b",
 
-    fullName: "Pham Hoai Duy",
-    email: "duythunglungtinhiu@gmail.com",
-    phoneNumber: "0555666612",
-    avatarUrl: null,
+//     fullName: "Pham Hoai Duy",
+//     email: "duythunglungtinhiu@gmail.com",
+//     phoneNumber: "0555666612",
+//     avatarUrl: null,
 
-    role: "User",
-    status: true,
+//     role: "User",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "0075ba2c-f60d-4f75-b9f1-f71579bc4fd2",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "0075ba2c-f60d-4f75-b9f1-f71579bc4fd2",
 
-    fullName: "Nguyen Pham Khanh",
-    email: "khanhpham@gmail.com",
-    phoneNumber: "0969998878",
-    avatarUrl: null,
+//     fullName: "Nguyen Pham Khanh",
+//     email: "khanhpham@gmail.com",
+//     phoneNumber: "0969998878",
+//     avatarUrl: null,
 
-    role: "User",
-    status: true,
+//     role: "User",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "20041d5b-01e4-443f-9652-e39beb9748e5",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "20041d5b-01e4-443f-9652-e39beb9748e5",
 
-    fullName: "Van Huu Toan Con",
-    email: "consultant@gmail.com",
-    phoneNumber: "0987654321",
-    avatarUrl: null,
+//     fullName: "Van Huu Toan Con",
+//     email: "consultant@gmail.com",
+//     phoneNumber: "0987654321",
+//     avatarUrl: null,
 
-    role: "Consultant",
-    status: true,
+//     role: "Consultant",
+//     status: true,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  },
-  {
-    userId: "4565f47a-7239-4666-b9b4-0523b1d9ba3d",
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   },
+//   {
+//     userId: "4565f47a-7239-4666-b9b4-0523b1d9ba3d",
 
-    fullName: "Quang La Tui",
-    email: "quangdalat@gmail.com",
-    phoneNumber: "0999777712",
-    avatarUrl: null,
+//     fullName: "Quang La Tui",
+//     email: "quangdalat@gmail.com",
+//     phoneNumber: "0999777712",
+//     avatarUrl: null,
 
-    role: "User",
-    status: false,
+//     role: "User",
+//     status: false,
 
-    createdAt: "2025-01-06T00:00:00",
-    updatedAt: "2025-01-06T00:00:00",
-    createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
-    updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
-  }
-]
+//     createdAt: "2025-01-06T00:00:00",
+//     updatedAt: "2025-01-06T00:00:00",
+//     createdBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+//     updatedBy: "3026595f-1414-4b74-be8f-11b7f6e7f4f6"
+//   }
+// ]
